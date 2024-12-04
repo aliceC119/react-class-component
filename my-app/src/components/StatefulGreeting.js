984b2ea534818c29770fcd3b3f5d1b4a62c1c678
Alice Chan <alicebay10@yahoo.co.uk>
2024-12-04 12:56:45 +0000

    Added an onClick attribute.

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -10,11 +10,16 @@ class StatefulGreeting extends React.Component {
 
         };
     }
+
+    handleClick() {
+        console.log('button clicked');
+    }
+    
     render() {
         return (
         <div>
             <h1>{this.state.introduction} {this.props.greeting}</h1>
-            <button>{this.state.buttonText}</button>
+            <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
         </div>
         )
     }
